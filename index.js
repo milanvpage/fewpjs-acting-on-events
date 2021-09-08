2d6062b85dfeb77c7cb5ad529b83f8ef06761e34
Milan <milanvpage@gmail.com>
2021-09-08 15:11:27 -0700

    commit tree e50ea1b365bd425d37fc7f8e98042e094313bf68

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+})
+
+//function moveDodgerRight() {
+  //  let rightNumbers = dodger.style.right.replace("px", "");
+    //let right = parseInt(rightNumbers, 10);
+
+    //if (right < 360) {
+        //dodger.style.right = `${right + 1}px`;
+
+    //}
+//}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 10}px`;
+    }
+  }
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
+
